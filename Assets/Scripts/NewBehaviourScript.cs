0664136f64af3b18081e645d0a2778dded5151e8
kostya <kostya12321@mail.ru>
2016-11-04 18:20:54 +0200

    New MurrMeow

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
-
+    void Start()
+    {
+        Debug.Log("MurrMeow");
+    }
 	void FixedUpdate () {
         this.gameObject.GetComponent<Renderer>().material.SetColor("_Color",Random.ColorHSV());
 	}
