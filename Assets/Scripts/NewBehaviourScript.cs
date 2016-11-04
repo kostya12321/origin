51412e7568ff008d09dc4c907c8cea80fe398a18
kostya <kostya12321@mail.ru>
2016-11-04 17:26:32 +0200

    Start

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void FixedUpdate () {
+        this.gameObject.GetComponent<Renderer>().material.SetColor("_Color",Random.ColorHSV());
+	}
+}
